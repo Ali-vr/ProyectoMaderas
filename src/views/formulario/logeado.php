011e8e66b2892144bdee00aba6dc345a2ab731b5
Ali-vr <alivergara24@gmail.com>
2026-06-10 21:15:56 -0300

    Add files via upload

---
+++ src/views/formulario/logeado.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <div class="row mb-3">
+        <label class="col-12 col-md-3 col-form-label">Nombre:</label>
+        <div class="col-12 col-md-9">
+            <a><?=$nombre ?></a>
+        </div>
+    </div>
+
+    <div class="row mb-3">
+        <label class="col-12 col-md-3 col-form-label">precio:</label>
+        <div class="col-12 col-md-9">
+            <a><?=$precio ?></a>
+        </div>
+    </div>
+
+    <div class="row mb-3">
+        <label class="col-12 col-md-3 col-form-label">descripcion:</label>
+        <div class="col-12 col-md-9">
+            <a><?=$descripcion?></a>
+        </div>
+    </div>
+
+</body>
+</html>
